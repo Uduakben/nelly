04fefb8eef5155b971f290bfb08540971cc0346d
Uduak Ben <uduakben11@gmail.com>
2023-08-16 19:29:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function mobileMenu (){
+    var x = document.getElementById("my-links");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    } else{
+        x.style.display = "block";
+    }
+}
